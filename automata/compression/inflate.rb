bb80a526a8d9c2536fa6b2945569956f108f851f
Vassilis S. Moustakas <vsmoustakas@gmail.com>
2012-12-01 21:55:49 +0200

    Tidying up code.

--- automata/compression/inflate.rb
+++ automata/compression/inflate.rb
@@ -7,6 +7,7 @@
 ################################################################################
 
 puts ""
+puts "Inflate 1.0     2012 Vassilis Moustakas"
 puts "Multi-type archive inflation script"
 puts "==================================="
 puts ""
@@ -15,48 +16,35 @@ archive = ARGV[0]
 name = File.basename(archive)
 extention = File.extname(archive)
 puts "Manipulating file " + name + " of extention " + extention
+success = false;
 case extention
 when ".zip"
     puts "Attempting to inflate zip file " + archive + "."
     cmd = "unzip " + archive
-    if system(cmd)
-        puts "Command " + cmd + " successfully executed!"
-    else
-        puts "Command " + cmd + " failed to execute!"
-    end
+    success = system(cmd)
 when ".rar"
     puts "Attempting to inflate rar file " + archive + "."
     cmd = "rar x " + archive
-    if system(cmd)
-        puts "Command " + cmd + " successfully executed!"
-    else
-        puts "Command " + cmd + " failed to execute!"
-    end
+    success = system(cmd)
 when ".gz"
     puts "Attempting to inflate gzipped file " + archive + "."
     cmd = "gunzip " + archive
-    if system(cmd)
-        puts "Command " + cmd + " successfully executed!"
-    else
-        puts "Command " + cmd + " failed to execute!"
-    end
+    success = system(cmd)
 when ".7z"
     puts "Attempting to inflate 7-zipped file " + archive + "."
     cmd = "7za x " + archive
-    if system(cmd)
-        puts "Command " + cmd + " successfully executed!"
-    else
-        puts "Command " + cmd + " failed to execute!"
-    end
+    success = system(cmd)
 when ".tgz"
 #when ".tar.gz"
     puts "Attempting to inflate gzipped tarball file " + archive + "."
     cmd = "tar xzvf " + archive
-    if system(cmd)
-        puts "Command " + cmd + " successfully executed!"
-    else
-        puts "Command " + cmd + " failed to execute!"
-    end
+    success = system(cmd)
 else
     puts "Cannot handle the extention type of archive " + archive + "."
+end
+
+if success
+    puts "Command " + cmd + " successfully executed!"
+else
+    puts "Command " + cmd + " failed to execute!"
 end
